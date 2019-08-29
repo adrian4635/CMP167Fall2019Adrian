2643467e4f05a07f06777ddde76299e2a6530777
adrian4635 <54686555+adrian4635@users.noreply.github.com>
2019-08-29 16:42:41 -0400

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.scanner;
+public class Welcome {
+	publiic static void main (String["] args) {
+	String welcome = "Welcome to java world";
+	System.out.println("welcome");
+	
+	Scanner input = new Scanner (System.in);
+	
+	System.out.println("What is your name? I am program");
+	
+	String name = input.nextLine();
+	
+	System.out.println("Nice to meet you,");
+}	
