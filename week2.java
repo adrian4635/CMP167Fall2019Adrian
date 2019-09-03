328425c4b5db086b2af36e65ebbc36c9ea22ffe1
adrian4635 <54686555+adrian4635@users.noreply.github.com>
2019-09-03 16:44:43 -0400

    Add files via upload

---
+++ week2.java
@@ -0,0 +1,21 @@
+public class MegaMillion {
+	public static void main (String[]args) {
+		Random randGen= new Random();
+		int rnum1=randGen.nextInt(60)+1;
+		System.out.println(randGen.nextInt(60)+1);
+		
+		//Declare variable to hold amount of money
+		//Declare variable for cost of ticket
+		
+		//Prompt the user for the amount
+		//	of dollars to bet on	
+		//store amount into amount of dollar
+		//	variable
+		
+		//Prompt the user to enter six numbers
+		//	Create a variable for each value 
+		
+		//Randomly generate six numbers and compare
+		
+	}
+}
