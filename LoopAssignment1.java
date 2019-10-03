46d4614a63194dc3abd67d7d101bdcb759d55f31
adrian4635 <54686555+adrian4635@users.noreply.github.com>
2019-10-03 16:31:28 -0400

    Add files via upload

---
+++ LoopAssignment1.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ * @author Adrian
+ *
+ */
+import java.util.Scanner;
+
+public class LoopAssignment1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int i=1;
+		
+		System.out.println("Enter a number n: ");
+		
+		System.out.println("Will print all postive numbers less than n ");
+		
+		int n = input.nextInt();
+		
+		while(i<n)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+		}
+		
+
+
