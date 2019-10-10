730e643a860df4ef7c22e34d48a85e2a67539594
adrian4635 <54686555+adrian4635@users.noreply.github.com>
2019-10-10 16:35:46 -0400

    Add files via upload

---
+++ MethodAssignment.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author Adrian
+ *
+ */
+public class MethodAssignment {
+	public static void main(String[] args) {}
+	public static int productThree(int num1,int num2,int num3){
+		return num1*num2*num3;
+		System.out.println(return);
+		}
+	public static int sumAll(int a, int b, int c) {
+		return 
+		
+		
+	}
+
+}
+
